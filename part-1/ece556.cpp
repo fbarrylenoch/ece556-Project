95a93da2236392990fdb68224dea2d3856788077
jtseibert <jacob.seibert@wisc.edu>
2016-04-26 14:59:32 -0500

    more tests

--- part-1/ece556.cpp
+++ part-1/ece556.cpp
@@ -1200,7 +1200,7 @@ int readBenchmark(const char *fileName, routingInst *rst){
                 printf("\tblockage (%d/%d) between (%d,%d)->(%d,%d) with cap %d\n",
                         i, num, p1->x, p1->y, p2->x, p2->y, updatedCap);
                 index = getIndex(*p1, *p2, rst);
-                printf("updating edgeCaps at index...\n");
+                printf("updating edgeCaps at: %d...\n", index[0]);
                 rst->edgeCaps[index[0]] = updatedCap;
                 printf("deleting points...\n");
                 delete p1;
